7f7981363a7300bbb92fc6d9c9cd8ece88bb36cf
Jayson Knight <jayson.knight@jaysonknight.com>
2023-02-01 01:10:30 -0500

    Created skeleton for module for ease of automation

--- AdGuard/Common/Scripts/update-adguard-publicUrl-2.ps1
+++ AdGuard/Common/Scripts/update-adguard-publicUrl-2.ps1
@@ -1,18 +1,18 @@
-﻿Clear-Host
-$NewUri = "https://linkip.adguard-dns.com/linkip/db94e3e9/8AdnEQlPCjyMaX74vTDZkraUDUYpCFiZ1tcH8dSk9VH"
-$CurrentDate = Get-Date -DisplayHint Time
-[int]$MinimumWaitTime = 1000
-[int]$WaitTime = 200
-do {
-    Write-Host "Allocated wait time is: $WaitTime ms"
-    $NewResponse = Invoke-WebRequest -Uri $NewUri # -PassThru # -OutFile $OutputFile
-    $elapsedTime = New-TimeSpan -Start($CurrentDate)
-    Write-Host $elapsedTime "TOTAL elapsed time since invocation"
-    Write-Host $NewResponse.Content -ForegroundColor Green
-    Write-Host $elapsedTime.TotalSeconds "seconds elapsed since $currentDate" -ForegroundColor Magenta
-    Write-Host "Public IP address has been updated $counter times." -ForegroundColor Blue
-    Write-Host 
-    Write-Host
-    Start-Sleep -Milliseconds $WaitTime
-    $counter++
-} until ($infinity)
+﻿# Clear-Host
+# $NewUri = "https://linkip.adguard-dns.com/linkip/db94e3e9/8AdnEQlPCjyMaX74vTDZkraUDUYpCFiZ1tcH8dSk9VH"
+# $CurrentDate = Get-Date -DisplayHint Time
+# [int]$MinimumWaitTime = 1000
+# [int]$WaitTime = 200
+# do {
+#     Write-Host "Allocated wait time is: $WaitTime ms"
+#     $NewResponse = Invoke-WebRequest -Uri $NewUri # -PassThru # -OutFile $OutputFile
+#     $elapsedTime = New-TimeSpan -Start($CurrentDate)
+#     Write-Host $elapsedTime "TOTAL elapsed time since invocation"
+#     Write-Host $NewResponse.Content -ForegroundColor Green
+#     Write-Host $elapsedTime.TotalSeconds "seconds elapsed since $currentDate" -ForegroundColor Magenta
+#     Write-Host "Public IP address has been updated $counter times." -ForegroundColor Blue
+#     Write-Host 
+#     Write-Host
+#     Start-Sleep -Milliseconds $WaitTime
+#     $counter++
+# } until ($infinity)
