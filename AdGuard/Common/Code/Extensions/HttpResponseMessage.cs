62f9331cafeb25ef6e64263e8d6bc06cb8c3652f
Jayson Knight <jayson.knight@jaysonknight.com>
2023-02-03 01:15:13 -0500

    Basic code project structure, initial checkin

---
+++ AdGuard/Common/Code/Extensions/HttpResponseMessage.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Code.Extensions;
+public static class HttpResponseMessageExtensions
+{
+    public static void WriteRequestToConsole(this HttpResponseMessage response, CancellationTokenSource cts, Stopwatch watch)
+    {
+        if (response is null)
+        {
+            return;
+        }
+        while (!cts.Token.IsCancellationRequested)
+        {
+            var request = response.RequestMessage;
+            Console.Write($"{request?.Method} ");
+            Console.Write($"{request?.RequestUri} ");
+            Console.Write($"{request?.Content} ");
+            Console.WriteLine($"HTTP/{request?.Version}");
+            Console.WriteLine($"status: {response.StatusCode}, via HTTP version: {response.Version}");
+            Console.WriteLine($"Response message: {response.Content} ");
+            Console.WriteLine($" Stopwatch elapsed {watch.Elapsed} milliseconds {watch.ElapsedMilliseconds} and ticks {watch.ElapsedTicks}");
+            Console.WriteLine(); Console.WriteLine();
+        }
+    }
+}
