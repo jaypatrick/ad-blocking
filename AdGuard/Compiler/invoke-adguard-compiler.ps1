7bf53938b1fc3a94629f5949b8881faf2eccc89a
Jayson Knight <jayson.knight@jaysonknight.com>
2025-03-05 20:59:02 -0500

    Small cleanup to execution script

--- AdGuard/Compiler/invoke-adguard-compiler.ps1
+++ AdGuard/Compiler/invoke-adguard-compiler.ps1
@@ -1,9 +1,10 @@
+# prior to running this script, use npm to install hostlist-compiler globally (npm install -g hostlist-compiler)
 $workingDirectory = D:\source\repos\ad-blocking\AdGuard\Compiler\
 $outputFileName = adguard_user_filter.txt
 $configFileName = config.json
 
-cd $workingDirectory
-hostlist-compiler --config config.json --output $outputFileName
+Set-Location -Path $workingDirectory
+hostlist-compiler --config $configFileName --output $outputFileName
 $outputFileHash = get-filehash $outputFileName -Algorithm SHA384
 write-host $outputFileHash
 
