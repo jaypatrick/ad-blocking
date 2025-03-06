11daae164fec70bfffc43b7da1e43ca85a329ee6
Jayson Knight <jayson.knight@jaysonknight.com>
2025-03-05 20:41:34 -0500

    Updated execution script to copy rules list to location referenced by bit.ly URL

--- AdGuard/Compiler/invoke-adguard-compiler.ps1
+++ AdGuard/Compiler/invoke-adguard-compiler.ps1
@@ -1,10 +1,13 @@
 $workingDirectory = D:\source\repos\ad-blocking\AdGuard\Compiler\
-$outputFileName = adguard_user_filter-txt
+$outputFileName = adguard_user_filter.txt
 $configFileName = config.json
 
 cd $workingDirectory
 hostlist-compiler --config config.json --output $outputFileName
 $outputFileHash = get-filehash $outputFileName -Algorithm SHA384
 write-host $outputFileHash
+
+# Copy to Rules directory
+Copy-Item -Path $outputFileName -Destination D:\source\repos\ad-blocking\AdGuard\Rules\adguard_user_filter.txt -Force
 
 pause 
