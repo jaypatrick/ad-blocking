b12e32af68b818fb187996b9632fd83b1f3a247c
Jayson Knight <jayson.knight@jaysonknight.com>
2025-11-26 13:54:22 -0500

    Update curl command to use a webhook endpoint
    Signed-off-by: Jayson Knight <jayson.knight@jaysonknight.com>

--- scripts/powershell/curl.cmd
+++ scripts/powershell/curl.cmd
@@ -2,4 +2,4 @@ curl \
     --connect-timeout 30 \
     --retry 300 \
     --retry-delay 5 \
-    https://linkip.adguard-dns.com/linkip/db94e3e9/8AdnEQlPCjyMaX74vTDZkraUDUYpCFiZ1tcH8dSk9VH
+    https://<your webhook endpoint>
